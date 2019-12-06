c05d1041e87f44fa5945627e9d5fd516ee273cc5
HocoHelper <58593738+HocoHelper@users.noreply.github.com>
2019-12-06 15:26:18 +0330

    End project

--- source.py
+++ source.py
@@ -1,101 +1,42 @@
-import math
 import csv
-from statistics import mean
-import operator
+import itertools
 from collections import OrderedDict
+import hashlib
 
-def calculate_averages(input_file_name, output_file_name):
-    with open(input_file_name, newline='') as f:
+def hash_password_hack(input_file_name, output_file_name):
+    with open (input_file_name, 'r') as f:
         reader = csv.reader(f)
-        f = open(output_file_name, "w")
-        for row in reader:
-            name=row[0]
-            numbers=list()
-            for number in row[1:]:
-                numbers.append(int(number))
-            average=float(mean(numbers))
-            f.write(name+',' )
-            f.write(str(average)+'\n')
-        f.close()
-            
-
-
-def calculate_sorted_averages(input_file_name, output_file_name):
-    with open(input_file_name, newline='') as f:
-        reader = csv.reader(f)
-        f = open(output_file_name, "w")
-        num=[]
-        names=[]
-        for row in reader:
-            name=row[0]
-            numbers=list()
-            for number in row[1:]:
-                numbers.append(int(number))
-            names.append(name)
-            num.append(float(mean(numbers)))
-            dictionary = OrderedDict(zip(names, num))
-        sorted_d = sorted(dictionary.items(),key=lambda t: t[::-1])
-        for x,y in sorted_d:
-            f.write(str(x)+',' )
-            f.write(str(y)+'\n' )
-        f.close()
-
-def calculate_three_best(input_file_name, output_file_name):
-    with open(input_file_name,newline='') as f:
-        reader=csv.reader(f)
         f = open(output_file_name,"w")
-        num=[]
-        names=[]
+        key=[]
+        val1=[]
         for row in reader:
-            name=row[0]
-            numbers=list()
-            for number in row[1:]:
-                numbers.append(int(number))
-            names.append(name)
-            num.append(float(mean(numbers)))
-            dictionary = OrderedDict(zip(names, num))
-        sorted_d = sorted(dictionary.items(),key=lambda t: t[::-1])
-        rev=list(reversed(sorted_d))
-        for x,y in rev[:3]:
-            f.write(str(x)+',' )
-            f.write(str(y)+'\n' )
-        f.close()
+            name = row[0]
+            val=[str(num) for num in row[1:]]
+            key.append(name)
+            val1.append(val)
+            value = list(itertools.chain.from_iterable(val1)) 
+            value=[str(i) for i in value]
+        dict_csv = OrderedDict(zip(key, value))  
+    listval=[]
+    for item in dict_csv.values():
+        listval.append(item)
+    rainbow_dict = OrderedDict()
+    for i in range(1000, 10000): 
+        my_hash = hashlib.sha256(str(i).encode()).hexdigest()
+        rainbow_dict[my_hash] = i
 
+    list1=[k for k,v in dict_csv.items() if v in listval]
+    list2=[v for k,v in rainbow_dict.items() if k in listval]
+    list3 = [ item for pair in zip(list1, list2 + [0]) for item in pair]
+    data=[]
+    for item in list3:
+        l3=[]
+        l3.append(item)
+        data.append(str(item))
+    esm=data[::2]
+    pas=data[1::2]
+    XY = [i for i in zip(esm, pas)]
+    for x,y in XY:
+        f.write(str(x)+',')
+        f.write(str(y)+'\n')
 
-def calculate_three_worst(input_file_name, output_file_name):
-    with open(input_file_name,newline='') as f:
-        reader=csv.reader(f)
-        f = open(output_file_name,"w")
-        num=[]
-        names=[]
-        for row in reader:
-            name=row[0]
-            numbers=list()
-            for number in row[1:]:
-                numbers.append(int(number))
-            names.append(name)
-            num.append(float(mean(numbers)))
-            dictionary = OrderedDict(zip(names, num))
-        sorted_d = sorted(dictionary.items(),key=lambda t: t[::-1])
-        for x,y in sorted_d[:3]:
-            f.write(str(y)+'\n' )
-        f.close()
-
-
-def calculate_average_of_averages(input_file_name, output_file_name):
-    with open(input_file_name,newline='') as f:
-        reader=csv.reader(f)
-        f = open(output_file_name,"w")
-        num=[]
-        names=[]
-        for row in reader:
-            name=row[0]
-            numbers=list()
-            for number in row[1:]:
-                numbers.append(int(number))
-            names.append(name)
-            num.append(float(mean(numbers)))
-        all_ave=float(mean(num))
-        f.write(str(all_ave))
-        f.close()
-        
